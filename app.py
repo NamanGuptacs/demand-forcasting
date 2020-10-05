72575087231134d58adca268b31f056bdb22706e
Naman Gupta <namangupta.cs@gmail.com>
2020-10-05 18:52:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def predict():
 
     output = round(prediction[0], 2)
 
-#     return render_template('index.html', prediction_text=output)
-    return redirect(url_for('index') + '#myModal', prediction_text=output)
+    return render_template('index.html', prediction_text=output,show_predictions_modal=True)
+#     return redirect(url_for('index') + '#myModal', prediction_text=output)
 
 if __name__ == "__main__":
     app.run(debug=True)
